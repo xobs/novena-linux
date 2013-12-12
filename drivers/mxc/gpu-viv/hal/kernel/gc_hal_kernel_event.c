22507f005e6fbab46d286e90cbbcbd5b879589f6
Russell King <rmk+kernel@arm.linux.org.uk>
2013-12-12 13:33:06 +0100

    GALCORE: fix race in _IsEmpty()
    The following race is possible:
    
    Thread 0                        Thread 1
    Read Event->queues[0].head
    Finds that it's NULL
                                    Acquires Event->mutexQueue
                                    Sets Event->queues[0].head
                                    Releases Event->mutexQueue
    Acquires Event->mutexQueue
    Releases Event->mutexQueue
    IsEmpty = True
    
    Therefore, it is possible that this function returns with IsEmpty=TRUE
    even though the event queues may not be empty.  The converse is also
    possible:
    
    Thread 0                        Thread 1 (event thread)
    Read Event->queues[0].head
    Finds that it's non-NULL
                                    Receives interrupt
                                    Acquires Event->mutexQueue
                                    Clears Event->queues[0].head
                                    Releases Event->mutexQueue
    Acquires Event->mutexQueue
    Releases Event->mutexQueue
    IsEmpty = False
    
    Here, the queues are empty, but we've returned that they not empty.
    
    Fix this by reading the Event queue pointers beneath the lock.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Adapted to the V4 codebase
    
    Signed-off-by: Jon Nettleton <jon.nettleton@gmail.com>

--- drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_event.c
+++ drivers/mxc/gpu-viv/hal/kernel/gc_hal_kernel_event.c
@@ -156,18 +156,6 @@ gckEVENT_IsEmpty(
     /* Assume the event queue is empty. */
     *IsEmpty = gcvTRUE;
 
-    /* Walk the event queue. */
-    for (i = 0; i < gcmCOUNTOF(Event->queues); ++i)
-    {
-        /* Check whether this event is in use. */
-        if (Event->queues[i].head != gcvNULL)
-        {
-            /* The event is in use, hence the queue is not empty. */
-            *IsEmpty = gcvFALSE;
-            break;
-        }
-    }
-
     /* Try acquiring the mutex. */
     status = gckOS_AcquireMutex(Event->os, Event->eventQueueMutex, 0);
     if (status == gcvSTATUS_TIMEOUT)
@@ -179,6 +167,18 @@ gckEVENT_IsEmpty(
     {
         /* Bail out on error. */
         gcmkONERROR(status);
+
+        /* Walk the event queue. */
+        for (i = 0; i < gcmCOUNTOF(Event->queues); ++i)
+        {
+            /* Check whether this event is in use. */
+            if (Event->queues[i].head != gcvNULL)
+            {
+                /* The event is in use, hence the queue is not empty. */
+                *IsEmpty = gcvFALSE;
+                break;
+            }
+        }
 
         /* Release the mutex. */
         gcmkVERIFY_OK(gckOS_ReleaseMutex(Event->os, Event->eventQueueMutex));
