fc8ff36f982de5611938d457754794f816c7e007
Rashika Kheria <rashika.kheria@gmail.com>
2013-12-13 12:59:52 +0530

    drivers: platform: Include appropriate header file in mxm-wmi.c
    This patch includes appropriate header file linux/mxm-wmi.h in
    x86/mxm-wmi.c because functions mxm_wmi_call_mxds(), mxm_wmi_call_mxmx()
    and mxm_wmi_supported() have their prototype declaration in
    linux/mxm-wmi.h.
    
    Thus, it also eliminates the following warnings in x86/mxm-wmi.c:
    drivers/platform/x86/mxm-wmi.c:43:5: warning: no previous prototype for ‘mxm_wmi_call_mxds’ [-Wmissing-prototypes]
    drivers/platform/x86/mxm-wmi.c:68:5: warning: no previous prototype for ‘mxm_wmi_call_mxmx’ [-Wmissing-prototypes]
    drivers/platform/x86/mxm-wmi.c:93:6: warning: no previous prototype for ‘mxm_wmi_supported’ [-Wmissing-prototypes]
    
    Signed-off-by: Rashika Kheria <rashika.kheria@gmail.com>
    Signed-off-by: Matthew Garrett <matthew.garrett@nebula.com>

--- drivers/platform/x86/mxm-wmi.c
+++ drivers/platform/x86/mxm-wmi.c
@@ -20,7 +20,9 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
-#include <linux/acpi.h>
+#include <linux/mxm-wmi.h>
+#include <acpi/acpi_bus.h>
+#include <acpi/acpi_drivers.h>
 
 MODULE_AUTHOR("Dave Airlie");
 MODULE_DESCRIPTION("MXM WMI Driver");
