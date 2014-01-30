64f8afa683072b34c3d89a33960d9535a65a1cc5
Russell King <rmk+kernel@arm.linux.org.uk>
2014-01-30 12:14:28 +0800

    regulator: allow GPIO 0 to be used for an enable signal
    GPIO number 0 *is* legal and must be accepted.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/regulator/fixed.c
+++ drivers/regulator/fixed.c
@@ -163,9 +163,7 @@ static int reg_fixed_voltage_probe(struct platform_device *pdev)
 		drvdata->desc.n_voltages = 1;
 
 	drvdata->desc.fixed_uV = config->microvolts;
-
-	if (config->gpio >= 0)
-		cfg.ena_gpio = config->gpio;
+	cfg.ena_gpio = config->gpio;
 	cfg.ena_gpio_invert = !config->enable_high;
 	if (config->enabled_at_boot) {
 		if (config->enable_high)
