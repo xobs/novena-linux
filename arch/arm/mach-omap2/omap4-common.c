b42fd76b998197f5ec482b7bbf80a0a0d43153c3
Russell King <rmk+kernel@arm.linux.org.uk>
2014-04-24 18:24:19 +0800

    ARM: l2c: omap2: remove explicit SMI calls to enable L2 cache
    Now that OMAP2 uses the write_sec method, we don't need to enable the L2
    cache in OMAP2 specific code; this can be done via the normal mechanisms
    in the L2C code.  Remove the OMAP2 specific code.
    
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mach-omap2/omap4-common.c
+++ arch/arm/mach-omap2/omap4-common.c
@@ -223,11 +223,6 @@ static int __init omap_l2_cache_init(void)
 			(1 << L2X0_AUX_CTRL_INSTR_PREFETCH_SHIFT) |
 			(1 << L2X0_AUX_CTRL_EARLY_BRESP_SHIFT);
 
-	omap_smc1(0x109, aux_ctrl);
-
-	/* Enable PL310 L2 Cache controller */
-	omap_smc1(0x102, 0x1);
-
 	outer_cache.write_sec = omap4_l2c310_write_sec;
 	if (of_have_populated_dt())
 		l2x0_of_init(aux_ctrl, L2X0_AUX_CTRL_MASK);
