3139a60d2281f9fec2b72ce65b04f40015d881f3
Russell King <rmk+kernel@arm.linux.org.uk>
2015-03-10 16:30:09 +0000

    staging: etnaviv: rename last remaining msm_* symbols
    Rename the last three msm_* symbols with an etnaviv_ prefix.  This
    resolves some build errors when MSM and Etnaviv DRM drivers are both
    built into the same kernel.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/staging/etnaviv/etnaviv_drv.h
+++ drivers/staging/etnaviv/etnaviv_drv.h
@@ -82,11 +82,11 @@ int etnaviv_gem_get_iova(struct etnaviv_gpu *gpu, struct drm_gem_object *obj,
 void etnaviv_gem_put_iova(struct drm_gem_object *obj);
 struct sg_table *etnaviv_gem_prime_get_sg_table(struct drm_gem_object *obj);
 void *etnaviv_gem_prime_vmap(struct drm_gem_object *obj);
-void msm_gem_prime_vunmap(struct drm_gem_object *obj, void *vaddr);
+void etnaviv_gem_prime_vunmap(struct drm_gem_object *obj, void *vaddr);
 struct drm_gem_object *etnaviv_gem_prime_import_sg_table(struct drm_device *dev,
 	struct dma_buf_attachment *attach, struct sg_table *sg);
-int msm_gem_prime_pin(struct drm_gem_object *obj);
-void msm_gem_prime_unpin(struct drm_gem_object *obj);
+int etnaviv_gem_prime_pin(struct drm_gem_object *obj);
+void etnaviv_gem_prime_unpin(struct drm_gem_object *obj);
 void *etnaviv_gem_vaddr_locked(struct drm_gem_object *obj);
 void *etnaviv_gem_vaddr(struct drm_gem_object *obj);
 dma_addr_t etnaviv_gem_paddr_locked(struct drm_gem_object *obj);
