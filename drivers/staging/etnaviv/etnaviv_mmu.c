3717119430c0ab231ab6e47630e74bdad5f6f71d
Russell King <rmk+kernel@arm.linux.org.uk>
2015-06-02 16:48:46 +0800

    staging: etnaviv: publish and use mmu geometry
    We model the GPU MMU using the iommu layer, which supports exporting
    the iommu domain geometry.  Use this feature to publish the size of
    the MMU window, and initialise the MMU drm_mm object according to
    the available MMU window size.
    
    As we only allocate a MMU page table which covers 256MB, yet we
    initialised the drm_mm object to cover 1GB, this fixes an overflow
    of the MMU page table array.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/staging/etnaviv/etnaviv_mmu.c
+++ drivers/staging/etnaviv/etnaviv_mmu.c
@@ -110,7 +110,9 @@ struct etnaviv_iommu *etnaviv_iommu_new(struct drm_device *dev,
 	mmu->dev = dev;
 	mmu->version = version;
 
-	drm_mm_init(&mmu->mm, 0x80000000, SZ_1G);
+	drm_mm_init(&mmu->mm, domain->geometry.aperture_start,
+		    domain->geometry.aperture_end -
+		      domain->geometry.aperture_start + 1);
 
 	iommu_set_fault_handler(domain, etnaviv_fault_handler, dev);
 
