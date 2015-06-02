87b008eeadde49e2e18fc17eff48c739c11f97bb
Russell King <rmk+kernel@arm.linux.org.uk>
2015-06-02 16:49:11 +0800

    staging: etnaviv: fix event allocation failure path
    If we fail to allocate an event, we leave the submitted fence number
    incremented.  This can cause an already running hangcheck timer to
    believe that we should be waiting for further events when no event has
    actually been queued.
    
    Resolve this by moving the fence allocation (which can never fail)
    after the event allocation.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/staging/etnaviv/etnaviv_gpu.c
+++ drivers/staging/etnaviv/etnaviv_gpu.c
@@ -921,12 +921,7 @@ int etnaviv_gpu_submit(struct etnaviv_gpu *gpu,
 {
 	struct drm_device *dev = gpu->drm;
 	struct etnaviv_drm_private *priv = dev->dev_private;
-	int ret = 0;
 	unsigned int event, i;
-
-	submit->fence = ++priv->next_fence;
-
-	gpu->submitted_fence = submit->fence;
 
 	/*
 	 * TODO
@@ -940,9 +935,12 @@ int etnaviv_gpu_submit(struct etnaviv_gpu *gpu,
 	event = event_alloc(gpu);
 	if (unlikely(event == ~0U)) {
 		DRM_ERROR("no free event\n");
-		ret = -EBUSY;
-		goto fail;
+		return -EBUSY;
 	}
+
+	submit->fence = ++priv->next_fence;
+
+	gpu->submitted_fence = submit->fence;
 
 	etnaviv_buffer_queue(gpu, event, submit);
 
@@ -973,8 +971,7 @@ int etnaviv_gpu_submit(struct etnaviv_gpu *gpu,
 	}
 	hangcheck_timer_reset(gpu);
 
-fail:
-	return ret;
+	return 0;
 }
 
 /*
