ef79ee5876027dfa7e8e29cdeab129edb30f01e5
Russell King <rmk+kernel@arm.linux.org.uk>
2015-07-20 15:27:53 +0800

    staging: etnaviv: clean up public API (part 2)
    This fixes the preceding patch for updates I have in my tree.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- drivers/staging/etnaviv/etnaviv_gem_submit.c
+++ drivers/staging/etnaviv/etnaviv_gem_submit.c
@@ -259,13 +259,6 @@ static int submit_reloc(struct etnaviv_gem_submit *submit, struct etnaviv_gem_ob
 			return -EINVAL;
 		}
 
-		/* Vivante hardware has no need for shifts or bitwise or-ing */
-		if (submit_reloc.shift || submit_reloc.or) {
-			DRM_ERROR("invalid shift or bitwise or at reloc %u\n",
-				  i);
-			return -EINVAL;
-		}
-
 		ret = submit_bo(submit, submit_reloc.reloc_idx, &bobj, &iova);
 		if (ret)
 			return ret;
