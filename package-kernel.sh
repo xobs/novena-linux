c2e899eb29336deb3dda556272ee8b52f79fb34c
Sean Cross <xobs@kosagi.com>
2016-01-13 17:29:41 +0800

    package-kernel: update to v4.4-novena-r2
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- package-kernel.sh
+++ package-kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # A script to build the kernel .deb files, as used by Kosagi.
 threads=$(($(grep '^processor' /proc/cpuinfo  | cut -d: -f2 | tail -n1)+1))
-version=4.4-novena-r1
+version=4.4-novena-r2
 
 echo "Building v${version} with ${threads} threads"
 git tag -d "v${version}" 2> /dev/null
