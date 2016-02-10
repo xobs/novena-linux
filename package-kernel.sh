a5fcfa9f2dfef653dc5a3d1d8608e86fbf2429cc
Sean Cross <xobs@kosagi.com>
2016-02-10 18:12:40 +0800

    package-kernel: make defconfig on build
    This will ensure developers have the correct config file.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- package-kernel.sh
+++ package-kernel.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 # A script to build the kernel .deb files, as used by Kosagi.
 threads=$(($(grep '^processor' /proc/cpuinfo  | cut -d: -f2 | tail -n1)+1))
 version=4.4-novena-r4
@@ -12,6 +12,9 @@ fi
 echo "Building v${version} with ${threads} threads"
 git tag -d "v${version}" 2> /dev/null
 git tag "v${version}"
+
+make novena_defconfig
+
 make -j${threads} \
 	KBUILD_DEBARCH=armhf \
 	KBUILD_IMAGE=zImage \
@@ -22,7 +25,7 @@ make -j${threads} \
 	KDEB_SOURCENAME="linux-image-novena" \
 	EMAIL="xobs@kosagi.com" \
 	NAME="Sean Cross" \
-	dtbs || exit 1
+	dtbs
 
 # Delete the debian "files" listing, as it tends to be out of date
 rm -f debian/files
