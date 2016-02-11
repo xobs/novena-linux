6b2254ce3fa51b93de3cdafea21a22c29a020b52
Sean Cross <xobs@kosagi.com>
2016-02-11 16:02:24 +0800

    package-kernel: update to 4.4-novena-r7
    This version tries to fix the ldb some more by restarting the
    ldb and display chips on boot.
    
    Signed-off-by: Sean Cross <xobs@kosagi.com>

--- package-kernel.sh
+++ package-kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # A script to build the kernel .deb files, as used by Kosagi.
 threads=$(($(grep '^processor' /proc/cpuinfo  | cut -d: -f2 | tail -n1)+1))
-version=4.4-novena-r6
+version=4.4-novena-r7
 
 if git diff-index --name-only HEAD | grep -qv "^scripts/package"; then
 	echo "Repo has local changes.  Stash or commit them."
